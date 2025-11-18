96b48b5ba4fa4ce74571859e20ff7d3149b89ed4
Oleksii_K <sumyultras88@gmail.com>
2025-11-18 15:27:07 +0200

    Доробив дерево підкатегорій в інструкціях

--- chatbot_project/backend/instructions_app/admin.py
+++ chatbot_project/backend/instructions_app/admin.py
@@ -1,30 +1,25 @@
 from django.contrib import admin
-from .models import InstructionCategory, InstructionSubcategory, Instruction, Tag
+from .models import InstructionNode, Instruction, Tag
 from audittrail.admin_mixins import AuditedModelAdmin
 
 
-@admin.register(InstructionCategory)
-class InstructionCategoryAdmin(AuditedModelAdmin):
-    list_display = ('name',)
-    search_fields = ('name',)
-
-
-@admin.register(InstructionSubcategory)
-class InstructionSubcategoryAdmin(AuditedModelAdmin):
-    list_display = ('name', 'category')
-    list_filter = ('category',)
-    search_fields = ('name',)
+@admin.register(InstructionNode)
+class InstructionNodeAdmin(AuditedModelAdmin):
+    list_display = ("name", "parent", "is_root")
+    list_filter = ("parent",)
+    search_fields = ("name",)
+    ordering = ("parent_id", "name")
 
 
 @admin.register(Instruction)
 class InstructionAdmin(AuditedModelAdmin):
-    list_display = ('title', 'subcategory')
-    list_filter = ('subcategory',)
-    search_fields = ('title', 'content')
-    filter_horizontal = ('tags',)
+    list_display = ("title", "node")
+    list_filter = ("node", "tags")
+    search_fields = ("title", "content")
+    filter_horizontal = ("tags",)
 
 
 @admin.register(Tag)
 class TagAdmin(AuditedModelAdmin):
-    list_display = ('name',)
-    search_fields = ('name',)
+    list_display = ("name",)
+    search_fields = ("name",)
